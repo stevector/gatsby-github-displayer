72c1e3368a4acb70e1a2f25c30aff37bd82ca60e
Steve Persch <steve.persch@pantheon.io>
2018-10-01 16:15:00 -0500

    github query for repo status

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -6,6 +6,11 @@ const RepositoryList = ({ repositories }) => (
       <div key={i}>
         <h2>{repository.node.name}</h2>
         url: {repository.node.url}
+        
+        defaultBranch: {repository.node.defaultBranchRef.name}
+        <br />
+        status: {repository.node.defaultBranchRef.target.status.state}
+        <br />
       </div>
     ))}
   </div>
