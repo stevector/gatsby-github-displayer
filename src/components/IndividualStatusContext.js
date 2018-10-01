a2519f07d5e8dfdd351e65e09bac2b4af0ba150a
Steve Persch <steve.persch@pantheon.io>
2018-10-01 17:11:13 -0500

    bg colors

--- src/components/IndividualStatusContext.js
+++ src/components/IndividualStatusContext.js
@@ -1,14 +1,34 @@
 import React from "react"
 
-const IndividualStatusContext = ({ statusContext }) => (
+const IndividualStatusContext = function ( statusContext ) {
+
+  console.log(statusContext);
   
-      <div>
-        <h5><a href="{statusContext.targetUrl}">{statusContext.state}: {statusContext.context}</a></h5>
-        createdAt: {statusContext.createdAt}
+  var divStyle = {backgroundColor: "red"};
+  switch (statusContext.statusContext.state) {
+    case 'ERROR':
+    case 'FAILURE':
+        divStyle.backgroundColor = "red";
+        break;
+    case 'PENDING':
+    divStyle.backgroundColor = "yellow";
+        break;
+    case 'SUCCESS':
+    divStyle.backgroundColor = "green";
+        break;
+    default:
+    divStyle.backgroundColor = "grey";
+  }
+
+  return (
+
+      <div style={divStyle}>
+        <h5><a href="{statusContext.statusContext.targetUrl}">{statusContext.statusContext.state}: {statusContext.statusContext.context}</a></h5>
+        createdAt: {statusContext.statusContext.createdAt}
       </div>
   
   
-)
+)}
 
 
 
