29b3ad69a8ee6fd25a50ef8b31957271df54e7dc
Steve Persch <steve.persch@pantheon.io>
2018-10-08 18:31:12 -0500

    repo as array

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,19 @@
-const path = require(`path`)
+const path = require(`path`);
+
+const repos = [
+  "repo:stevector/migrate_pantheon",
+  "repo:pantheon-systems/terminus-build-tools-plugin",
+  "repo:stevector/nerdologues-d8",
+  "repo:pantheon-systems/pantheon-wordpress-develop",
+  "repo:pantheon-systems/example-drops-8-composer",
+  "repo:pantheon-systems/drops-8",
+  "repo:pantheon-systems/wp-saml-auth",
+  "repo:pantheon-systems/wp-redis",
+  "repo:pantheon-systems/WordPress",
+  "repo:pantheon-systems/wp-native-php-sessions",
+  "repo:pantheon-systems/solr-power",
+  "repo:stevector/migrate_pantheon"
+];
 
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
@@ -6,14 +21,15 @@ exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
 
   return new Promise((resolve, reject) => {
-    const Dashboard = path.resolve(`src/components/Dashboard.js`)
+    const Dashboard = path.resolve(`src/components/Dashboard.js`);
+    const search_string = repos.join(' ');
     // Query for recipe nodes to use in creating pages.
     resolve(
       createPage({
         path: '/',
         component: Dashboard,
         context: {
-          searchstring:   "repo:stevector/migrate_pantheon          repo:pantheon-systems/terminus-build-tools-plugin   repo:stevector/nerdologues-d8          repo:pantheon-systems/pantheon-wordpress-develop         repo:pantheon-systems/example-drops-8-composer          repo:pantheon-systems/drops-8           repo:pantheon-systems/wp-saml-auth           repo:pantheon-systems/wp-redis          repo:pantheon-systems/WordPress          repo:pantheon-systems/wp-native-php-sessions          repo:pantheon-systems/solr-power          repo:stevector/migrate_pantheon"
+          searchstring:search_string
         },
       })
     )
