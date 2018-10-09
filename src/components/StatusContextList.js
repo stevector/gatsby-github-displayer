9ce126335a3e94549589573956c8b1469772a240
Steve Persch <steve.persch@pantheon.io>
2018-10-08 20:54:04 -0500

    using css

--- src/components/StatusContextList.js
+++ src/components/StatusContextList.js
@@ -1,19 +1,18 @@
 import React from "react"
+import './StatusContextList.css';
 import IndividualStatusContext from "./IndividualStatusContext"
 
  var divStyle = {
   //clear: 'both',
-  display: 'grid',
-  'grid-template-columns': '1fr 1fr 1fr 1fr',
-  width: '100%'
+
 };
 
 var divStyle2 = {
-  margin: '0 10px 10px 0',
+//  margin: '0 10px 10px 0',
 };
 
 const StatusContextList = ({ statusContexts }) => (
-  <div  style={divStyle} >
+  <div class="StatusContextList" style={divStyle} >
     {statusContexts.map((statusContext, i) => (
       <div key={i} style={divStyle2}>
         <IndividualStatusContext statusContext={statusContext} />
